6d3d955b241ba129b11efc0a94a11a0452e5531a
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 17:31:21 -0500

    Posted Results

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -29,7 +29,33 @@ class Results extends Component {
     render() {
         return (
             <div>
-
+                {!this.props.books.length ? (
+                    <h1 className="text-center">No Results to Display</h1>
+                ) : (
+                        <div>
+                            {this.props.books.map(result => (
+                                <div className="card mb-3" key={result._id}>
+                                    <div className="row">
+                                        <div className="col-md-2">
+                                            <img alt={result.title} className="img-fluid" src={result.image} />
+                                        </div>
+                                        <div className="col-md-10">
+                                            <div className="card-body">
+                                                <h5 className="card-title">{result.title} by {result.authors}</h5>
+                                                <p className="card-text">{result.description}</p>
+                                                <div>
+                                                    <a href={result.link} className="btn badge-pill btn-outline-dark mt-3" target="_blank" >View</a>
+                                                    <button onClick={() => this.handleSave(result)} className="btn badge-pill btn-outline-warning mt-3 ml-3" >
+                                                        {this.state.savedBooks.map(book => book._id).includes(result._id) ? "Unsave" : "Save"}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
             </div>
         )
     }
