8fe1237b1ee701c7466bf80617e5acc6fa6e1362
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 17:51:14 -0500

    Saved Books Appearing

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -3,6 +3,15 @@ import API from "../utils/API";
 import Results from "../components/Results";
 
 class Saved extends Component {
+    state = {
+        savedBooks: [],
+    }
+
+    componentDidMount() {
+        API.savedBooks()
+            .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+            .catch(err => console.error(err));
+    }
 
     render() {
         return (
