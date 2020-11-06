8328bd083866212f309fe3e63b10753b538086b8
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 15:46:56 -0500

    Added Row

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -8,4 +8,15 @@ export function Row({ fluid, children}) {
 return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
-
+export function Col({ size, children }) {
+    return (
+      <div
+        className={size
+          .split(" ")
+          .map(size => "col-" + size)
+          .join(" ")}
+      >
+        {children}
+      </div>
+    );
+}
