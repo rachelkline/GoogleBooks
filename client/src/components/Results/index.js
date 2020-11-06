470bf181433ff9e5eee1b19bdaf257ce264c2cd1
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 16:10:10 -0500

    Component to display results

---
+++ client/src/components/Results/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class Results extends Component {
+
+    state = {
+        savedBooks: [],
+    }
+
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default Results;
