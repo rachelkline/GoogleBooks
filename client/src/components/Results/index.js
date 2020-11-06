92097a3dd6705a32e5438154a3957fcf746c9b5e
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 17:30:14 -0500

    Functions for getting saved books and unsaving books

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -7,11 +7,30 @@ class Results extends Component {
         savedBooks: [],
     }
 
+    componentDidMount() {
+        API.savedBooks()
+            .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+            .catch(err => console.error(err));
+    }
+
+    handleSave = book => {
+
+        if (this.state.savedBooks.map(book => book._id).includes(book._id)) {
+            API.deleteBook(book._id)
+                .then(deletedBook => this.setState({ savedBooks: this.state.savedBooks.filter(book => book._id !== deletedBook._id) }))
+                .catch(err => console.error(err));
+        } else {
+            API.saveBook(book)
+                .then(savedBook => this.setState({ savedBooks: this.state.savedBooks.concat([savedBook]) }))
+                .catch(err => console.error(err));
+        }
+    }
 
     render() {
         return (
-            <>
-            </>
+            <div>
+
+            </div>
         )
     }
 }
