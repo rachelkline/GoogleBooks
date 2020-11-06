a1c42c89822cc50ffa75b1e7fa131a3b28d9ed77
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 15:45:26 -0500

    Added Row to Grid

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -4,4 +4,8 @@ export function Container({ fluid, children }) {
 return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
+export function Row({ fluid, children}) {
+return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
 
