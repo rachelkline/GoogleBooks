63d259afdce42f606badf5d408cae0203efdbf26
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 17:54:36 -0500

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import HomePage from "./pages/HomePage";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Container from "./components/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
-}
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
+import "./App.css";
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Container>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Container>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
