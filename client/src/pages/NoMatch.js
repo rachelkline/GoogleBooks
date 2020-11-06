4c9411a29af5a524877292669207def6a2865e0e
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 17:57:42 -0500

    Page for No Matches

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,10 +1,16 @@
 import React from "react";
-import Header from "../components/Header";
 
 function NoMatch() {
   return (
     <div>
-
+          
+            <h1>404 Page Not Found</h1>
+            <h1>
+              <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                🙄
+              </span>
+            </h1>
+          
     </div>
   );
 }
