1933154c4a303115f071cbc2ad0ce8faedee3e61
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 21:54:48 -0500

    Nav Styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,23 +7,25 @@ function Nav() {
     return (
 
 
-        <nav className="navbar navbar-expand-lg text-light">
-            <div className="container">
+        <nav className="nav-wrap">
+            
+                <div className="row">
                 <Link className="navbar-brand" to="/">Google Books</Link>
-                <ul className="navbar-nav">
+                <ul className="nav">
                     <li className="nav-item">
                         <Link
                             to="/search"
                             className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-                        > Search</Link>
+                        > search</Link>
                     </li>
                     <li className="nav-item">
                         <Link
                             to="/saved"
                             className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-                        >Saved</Link>
+                        >saved</Link>
                     </li>
                 </ul>
+          
             </div>
         </nav>
 
