3cfcea8cd87819ae3738b29049fc676cb1f2713e
Rachel Kline <rach3lkline@gmail.com>
2020-11-06 21:50:30 -0500

    Added Header Image

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./style.css";
+
+function Header() {
+    return (
+        <div className="header text-center">
+            <div className="text">
+                <h1 className="type">Google Book Search</h1>
+                <p>search & save books of interest!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
